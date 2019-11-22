0050f60478ef4791beb4bbc0cf967fc693a7dc4c
VnkHO <ho.vincent.nk@gmail.com>
2019-11-22 12:13:42 +0100

    arrow function pt 2, how it works

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', e => {
-  cursor.setAttribute("style", "top: " + (e.pageY - 14) + "px; left: " + (e.pageX - 14) + "px;")
+  cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
 })
 
 document.addEventListener('click', () => {
